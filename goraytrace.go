bfef4b3733c9f9f120ff32798da5f86c4d348575
Miguel Galindo Ramos <mjgalindoramos@gmail.com>
2017-06-30 20:44:01 +0200

    Add a sphere to the scene

--- goraytrace.go
+++ goraytrace.go
@@ -1,10 +1,17 @@
 package main
 
-import "github.com/ProjectMOA/goraytrace/scene"
-import "fmt"
+import (
+	"github.com/ProjectMOA/goraytrace/math3d"
+	"github.com/ProjectMOA/goraytrace/scene"
+	"github.com/ProjectMOA/goraytrace/shape"
+)
 
 func main() {
 	// Setting up a scene
-	myScene := scene.MakeScene()
-	fmt.Println(myScene.Elements())
+	myScene := scene.New()
+	aSphere := shape.Sphere{
+		Position: math3d.Vector3{X: 0.0, Y: 0.0, Z: 0.0},
+		Radius:   1.0}
+
+	myScene.Add(&aSphere)
 }
--- scene/scene.go
+++ scene/scene.go
@@ -7,12 +7,20 @@ type Scene struct {
 	spheres []*shape.Sphere
 }
 
-// MakeScene creates a new empty scene
-func MakeScene() *Scene {
+// New creates a new empty scene
+func New() *Scene {
 	return &Scene{spheres: make([]*shape.Sphere, 0, 10)}
 }
 
 // Elements returns the number of elements in the scene
 func (s *Scene) Elements() int {
 	return len(s.spheres)
+}
+
+// Add adds a sphere to the array of spheres in this scene.
+// Eventually, when go makes more sense, spheres should be
+// a list of shapes in some way so that all possible shapes
+// are stored in a unique block of memory.
+func (s *Scene) Add(sphere *shape.Sphere) {
+	s.spheres = append(s.spheres, sphere)
 }
